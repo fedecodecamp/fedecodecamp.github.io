7bf4a980a4a868505300a6f7d92b6b61fcd8f4c9
fedecodecamp <126691215+fedecodecamp@users.noreply.github.com>
2023-03-01 12:42:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById("dat").addEventListener('click', function () {
+    console.log("CLICK  utilizando addEventListener DATOS PERSONALES");
+   
+
+});   
+
+document.getElementById("conoc").addEventListener('click', function () {
+    console.log("CLICK  utilizando addEventListener CONOCIMIENTOS");
+    
+
+});   
+
+document.getElementById("exp").addEventListener('click', function () {
+    console.log("CLICK  utilizando addEventListener EXPERIENCIA");
+    
+
+});   
+
